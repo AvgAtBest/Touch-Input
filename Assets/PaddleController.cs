e8c747af862aaef82a71ff0f987808a24d19d937
DavidR1997 <david.rowsellwork@gmail.com>
2019-06-14 13:12:51 +1000

    bb assets

---
+++ Assets/PaddleController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+	public VariableJoystick joystick;
+
+	public float speed = 1f;
+	public Vector2 horizontalMove;
+	public Rigidbody2D rigid;
+	// Start is called before the first frame update
+	void Start()
+	{
+		//joystick = GetComponent<VariableJoystick>();
+		rigid = this.GetComponent<Rigidbody2D>();
+		rigid.isKinematic = false;
+		rigid.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+	}
+	private void Update()
+	{
+		
+		horizontalMove = JoyStickInput();
+		rigid.AddForce(horizontalMove, ForceMode2D.Impulse);
+	}
+	Vector2 JoyStickInput()
+	{
+		Vector2 inputDirection = joystick.GetAxis();
+		Vector2 newPos = Vector2.zero;
+		newPos.x = inputDirection.x;
+		newPos.y = inputDirection.y;
+		Debug.Log("Joystick" + joystick.input.x);
+
+		return newPos;
+
+	}
+
+}
