f1641d939cb902d9cd99765d55b0032dc969930c
DavidR1997 <david.rowsellwork@gmail.com>
2019-06-17 19:46:51 +1000

    something

---
+++ Assets/Scripts/Breakout/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+
+	public float speed = 5f;
+	public float verticleMove;
+	public float horizontalMove;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Respawn_Ball_Default();
+	}
+
+	// Update is called once per frame
+	public void Respawn_Ball_Default()
+	{
+		transform.position = Vector3.zero;
+		GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * speed;
+	}
+
+}
